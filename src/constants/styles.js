9d7033b219a63f0569e61a36abae242990cc79ce
Ayorinde <kudoroayorinde@gmail.com>
2019-01-26 08:50:31 +0100

    added hometab

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -3,6 +3,9 @@ const colors = {
     faintText: 'rgba(255, 255, 255, 0.3)', //white with opacity
     darkText: '#333333',//black
     light: '#fff',
+    cyan: '#EAFFF1',
+    dangerText: '#941D1B',
+    grayText: '#B5B5B4',
 
 }
 export { colors }
